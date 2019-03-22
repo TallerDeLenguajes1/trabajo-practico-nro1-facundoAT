7183515cf0f6168fe45564625b8677ffbf835acd
Facundo Toconás <facundotoconascabj27@gmail.com>
2019-03-22 07:54:03 -0300

    Una funcion fue agregada

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -10,39 +10,48 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <stdio.h>
+#include <stdlib.h>
 int cuadrado (int n); //i
-void cuadrado2(int *punt); //ii 
+void cuadrado2(int *punt); //ii
 void mostrar(int x); //iii
 void invertir(int *var,int *a); //iv
 void MayorMenor(int *var, int *a); //v
-
+void ordena(int var1, int var2);
 int main(void) {
-	int var,x;
-	int a;
-	puts("Ingrese 2 numeros");
+   int var,x;
+   int a;
+   int var1,var2;
+   puts("Ingrese 2 numeros");
    puts("El primero sera elevado el cuadrado");
    scanf("%d",&var);
    scanf("%d",&a);
 
-	x=cuadrado(var);
-	printf("i)Cuadrado %d",x);
+   x=cuadrado(var);
+   printf("i)Cuadrado %d",x);
 
-	cuadrado2(&var);
-	printf("\nii)Cuadrado %d",var);
+   cuadrado2(&var);
+   printf("\nii)Cuadrado %d",var);
 
-	printf("\nMando la variable elevada %d",var);
-	mostrar(var);
+   printf("\nMando la variable elevada %d",var);
+   mostrar(var);
 
-	printf("\niv)\nAntes de invertir a:%d b:%d",var ,a);
-	invertir(&var,&a);
-	printf("\nDespues de invertir a:%d b:%d",var ,a);
+   printf("\niv)\nAntes de invertir a:%d b:%d",var ,a);
+   invertir(&var,&a);
+   printf("\nDespues de invertir a:%d b:%d",var ,a);
 
-	printf("\nv)\nAntes de ordenar a:%d b:%d",var ,a);
-	MayorMenor(&var,&a);
-	printf("\nDespues de ordenar a:%d b:%d",var ,a);
+   printf("\nv)\nAntes de ordenar a:%d b:%d",var ,a);
+   MayorMenor(&var,&a);
+   printf("\nDespues de ordenar a:%d b:%d",var ,a);
 
+   printf("\nvi)\nIngrese 2 valores enteros\n");
+   scanf("%d",&var1);
+   scanf("%d",&var2);
 
-	return EXIT_SUCCESS;
+   ordena(var1,var2);
+   //printf("\nOrdenados :%d %d",var1 ,var2);
+   return EXIT_SUCCESS;
 }
 int cuadrado (int n){
   n=n*n;
@@ -63,10 +72,15 @@ void invertir(int *a,int *b){
    *b=aux;
 }
 void MayorMenor(int *var, int *a){
-   if(*var<*a){
+   if(*var>*a){
       invertir(var,a);
    }else{if(*var==*a){
       puts("\nSon valores iguales");
    }
    }
 }
+void ordena(int var1, int var2){
+   MayorMenor(&var1,&var2);
+   printf("\nOrdenados :%d %d",var1 ,var2);
+}
+
